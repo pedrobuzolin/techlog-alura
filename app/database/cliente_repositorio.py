eaa869db328fc7d0179ad54f4efec9e935c725f7
pedrobuzolin <buzolinpedro@gmail.com>
2026-04-08 09:46:00 -0300

    Criando o CRUD de clientes

--- app/database/cliente_repositorio.py
+++ app/database/cliente_repositorio.py
@@ -1,5 +1,5 @@
 from app.database.local import LocalDataBase
-from app.models.clientes import Cliente
+from app.models.clientes import Cliente, ClienteCriarAtualizar
 
 class ClienteRepositorio:
     def __init__(self, banco_dados: LocalDataBase):
@@ -21,3 +21,25 @@ class ClienteRepositorio:
             if linha:
                 return Cliente(id_=linha[0], nome=linha[1], email=linha[2], telefone=linha[3])
             return None
+        
+    async def criar_cliente(self, cliente: ClienteCriarAtualizar) -> Cliente:
+        with self.bd.conectar() as conexao:
+            cursor = conexao.cursor()
+            cursor.execute("INSERT INTO clientes (nome, email, telefone) VALUES (?, ?, ?)", (cliente.nome, cliente.email, cliente.telefone))
+            cliente_id = cursor.lastrowid
+            return Cliente(id_=cliente_id, nome=cliente.nome, email=cliente.email, telefone=cliente.telefone)
+        
+    async def atualizar_cliente(self, cliente_id: int, cliente: ClienteCriarAtualizar) -> Cliente | None:
+        with self.bd.conectar() as conexao:
+            cursor = conexao.cursor()
+            cursor.execute("UPDATE clientes SET nome = ?, email = ?, telefone = ? WHERE id = ?",
+                           (cliente.nome, cliente.email, cliente.telefone, cliente_id))
+            if cursor.rowcount == 0:
+                return None
+            return Cliente(id_=cliente_id, nome=cliente.nome, email=cliente.email, telefone=cliente.telefone)
+
+    async def deletar_cliente(self, cliente_id: int) -> bool:
+        with self.bd.conectar() as conexao:
+            cursor = conexao.cursor()
+            cursor.execute("DELETE FROM clientes WHERE id = ?", (cliente_id,))
+            return cursor.rowcount > 0
--- app/routes/cliente.py
+++ app/routes/cliente.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 from fastapi import APIRouter, Depends, HTTPException
-from app.models.clientes import Cliente
+from app.models.clientes import Cliente, ClienteCriarAtualizar
 from app.database.cliente_repositorio import ClienteRepositorio
 from app.dependencias import obter_cliente_repositorio
 
@@ -21,3 +21,24 @@ async def obter_clientes(cliente_repositorio: Annotated[ClienteRepositorio, Depe
         raise HTTPException(status_code=404, detail="Cliente não encontrado!")
     
     return cliente
+
+@router.post("/", response_model=Cliente, status_code=201)
+async def criar_cliente(cliente_repositorio: Annotated[ClienteRepositorio, Depends(obter_cliente_repositorio)], 
+                        cliente: ClienteCriarAtualizar):
+    return await cliente_repositorio.criar_cliente(cliente)
+
+@router.put("/{cliente_id}", response_model=Cliente | None)
+async def atualizar_cliente(cliente_repositorio: Annotated[ClienteRepositorio, Depends(obter_cliente_repositorio)],
+                            cliente_id: int,
+                            cliente: ClienteCriarAtualizar):
+    cliente_atualizado = await cliente_repositorio.atualizar_cliente(cliente_id, cliente)
+    if not cliente_atualizado:
+        raise HTTPException(status_code=404, detail="Cliente não encontrado!")
+    return cliente_atualizado
+
+@router.delete("/{cliente_id}", status_code=204)
+async def deletar_cliente(cliente_repositorio: Annotated[ClienteRepositorio, Depends(obter_cliente_repositorio)],
+                            cliente_id: int):
+    sucesso = await cliente_repositorio.deletar_cliente(cliente_id)
+    if not sucesso:
+        raise HTTPException(status_code=404, detail="Cliente não encontrado!")
